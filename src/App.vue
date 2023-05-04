74073fbb74a2cc5caf64d561715f494d5924f7c5
jyj <yunsky-@naver.com>
2023-05-04 15:38:25 +0900

    vue 개발환경 셋팅

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="menu">
+      <a v-for="(menu,index) in menu" :key="index">{{ menu }}</a>
+    </div>
+
+    <div v-for="(products,index) in products" :key="index" class="product">
+      <h4>{{ products.location }}</h4>
+      <p>{{ products.price }}</p>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      menu:['Home','Shop','About'],
+      products:[{location : '역삼동 원룸', price : '80만원'},{location : '논현동 원룸', price : '70만원'},{location : '계산동 원룸', price : '40만원'}]
+    }
+  },
+  components: { 
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color:  #2c3e50;
+  }
+  .menu{
+    background-color: darkslateblue ;
+    padding: 15px;
+    border-radius: 5px;
+  }
+  .menu a{
+    color: #FFF;
+    padding: 10px;
+  }
 </style>
