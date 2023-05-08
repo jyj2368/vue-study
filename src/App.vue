1a29c6a9c42a6b129d803a1ff334ec3618b06728
jyj <yunsky-@naver.com>
2023-05-08 17:31:14 +0900

    버튼 클릭 이벤트

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,21 @@
     <div v-for="(products,index) in products" :key="index" class="product">
       <h4>{{ products.location }}</h4>
       <p>{{ products.price }}</p>
+      <button @click="report(index)">허위매물 신고</button> <span>신고수 : {{ products.count }}</span>
     </div>
+    <!-- <div class="product">
+      <h4>{{ products[0].location }}</h4>
+      <p>{{ products[0].price }}</p>
+      <button @click="report">허위매물 신고</button> <span>신고수 : {{ count }}</span>
+    </div>
+    <div class="product">
+      <h4>{{ products[1].location }}</h4>
+      <p>{{ products[1].price }}</p>
+    </div>
+    <div class="product">
+      <h4>{{ products[2].location }}</h4>
+      <p>{{ products[2].price }}</p>
+    </div> -->
 
   </div>
 </template>
@@ -20,11 +34,18 @@ export default {
   data(){
     return{
       menu:['Home','Shop','About'],
-      products:[{location : '역삼동 원룸', price : '80만원'},{location : '논현동 원룸', price : '70만원'},{location : '계산동 원룸', price : '40만원'}]
+      products:[{location : '역삼동 원룸', price : '80만원',count : 0},{location : '논현동 원룸', price : '70만원',count : 0 },{location : '계산동 원룸', price : '40만원',count : 0}],
+      //count:0,
     }
   },
   components: { 
 
+  },
+  methods:{
+    report(idex){
+     const vm = this;
+      vm.products[idex].count ++;
+    }
   }
 }
 </script>
